151a206b8b6c281fff3046761ac3f7b71772a92c
tunafysh <hanansmani@gmail.com>
2026-04-29 14:45:06 +0200

    bruh

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -2,7 +2,43 @@ import NextAuth from "next-auth";
 import Credentials from "next-auth/providers/credentials";
 import Google from "next-auth/providers/google";
 import GitHub from "next-auth/providers/github";
-import { loginWithCredentials } from "./auth.service";
+import { createUser, getUserByEmail } from "@/db/queries/users";
+import argon2 from "argon2";
+
+async function loginWithCredentials(
+  credentials: Partial<Record<"email" | "password", string>>,
+) {
+  if (!credentials?.email || !credentials?.password) return null;
+
+  let user = await getUserByEmail(credentials.email);
+
+  if (!user) {
+    const hashed = await argon2.hash(credentials.password);
+
+    user = await createUser(
+      credentials.email.split("@")[0],
+      credentials.email,
+      hashed
+    );
+  }
+
+  if (!user?.passwordHash) return null;
+
+  const valid = await argon2.verify(
+    user.passwordHash,
+    credentials.password
+  );
+
+  if (!valid) return null;
+
+  return {
+    id: user.id,
+    email: user.email,
+    name: user.username,
+  };
+}
+
+
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [
     Credentials({
@@ -26,6 +62,7 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
         ) {
           return null;
         }
+
         return await loginWithCredentials({
           email: credentials.email as string,
           password: credentials.password as string,
