4ccca91ba8a4b47ae9b228962d219953e0fac561
tunafysh <hanansmani@gmail.com>
2026-05-08 00:12:01 +0200

    still debugging

--- src/components/elements/app-sidebar.tsx
+++ src/components/elements/app-sidebar.tsx
@@ -175,7 +175,7 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
         <NavSecondary items={data.navSecondary} className="mt-auto" />
       </SidebarContent>
       <SidebarFooter>
-        {/* <NavUser /> */}
+        <NavUser />
       </SidebarFooter>
     </Sidebar>
   );
--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -45,6 +45,14 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
   session: {
     strategy: "jwt",
   },
+  callbacks: {
+    async jwt({ token, user }) {
+      if (user) {
+        token.id = user.id;
+      }
+      return token;
+    },
+  },
   providers: [
     Credentials({
       name: "Cashly",
