3e42dd1302cef6297e3aba26343b3f52170a91a6
tunafysh <hanansmani@gmail.com>
2026-05-10 23:55:56 +0200

    fixed an edge case in the generateRandomColor function

--- src/db/queries/categories.ts
+++ src/db/queries/categories.ts
@@ -45,7 +45,9 @@ export async function createCategory({
 }
 
 function generateRandomColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, '0')}`;
 }
 
 export async function createCategoryWithoutColor(
