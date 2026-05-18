349dde25a5afa50b96d9c6957de71457dd0978fe
tunafysh <hanansmani@gmail.com>
2026-05-18 21:59:28 +0200

    idk

--- src/app/app/budget/page.tsx
+++ src/app/app/budget/page.tsx
@@ -1,7 +1,8 @@
 import { BudgetModifier } from "@/components/elements/uncategorized/budget-modifier";
 
 export default function Budget() {
-  return (<div className="pt-6 md:pt-4">
+  return (
+  <div className="pt-6 md:pt-4">
       <BudgetModifier />
     </div>);
 }
