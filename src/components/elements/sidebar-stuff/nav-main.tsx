32e277ab093d7e718b5e960d8f384dab6818a2ba
tunafysh <hanansmani@gmail.com>
2026-05-22 01:06:25 +0200

    bruh react is a footgun

--- src/components/elements/sidebar-stuff/nav-main.tsx
+++ src/components/elements/sidebar-stuff/nav-main.tsx
@@ -32,8 +32,10 @@ export function NavMain({
                 size="sm"
                 className="w-full bg-primary text-primary-foreground transition-all duration-200 hover:bg-primary/90 active:bg-primary/80 mt-8"
               >
+                <div className="flex items-center justify-center gap-2">
                   <CirclePlusIcon className="size-4" />
                   <span>Quick add</span>
+                </div>
               </Button>
             </QuickAddDialog>
           </SidebarMenuItem>
